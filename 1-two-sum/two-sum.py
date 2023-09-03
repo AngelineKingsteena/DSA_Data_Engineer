6982447ba48e99bb73e1c423c1900937f72caa1c
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-03 19:35:04 +0530

    Time: 74 ms (56.91%) | Memory: 17.5 MB (56.73%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,22 @@
 class Solution:
    def twoSum(self, nums, target):
-    for i in range(len(nums)):
-        r = target - nums[i]
-        if r in nums[i+1:]:
-            ind=nums[i+1:].index(r)
-            return [i,ind+i+1]
+        seen = {}
+        for i, value in enumerate(nums):
+            remaining = target - value
+        ###In each iteration check if required number (required  number = target sum - current number) is
+        ###present in the hashmap.
+
+        ###If present, return {required number index, current number index} as  result.
+
+        ###Otherwise add the current iteration number as key and its index as value to the hashmap.
+        ###Repeat this  until you find the result.
+
+            if remaining in seen:
+                # print(i,value,remaining)
+                return [min(i,nums.index(remaining)), max(i,seen[remaining])] #4,8 (earliest)
+    #             return [i+1, seen[remaining]+1]  # [8,6]  latest
+
+            seen[value] = i
+        return []
+
+#twoSum([ 4, 7, -4, 2, 2, 2, 3, -5, -3, 9, -4, 9, -7, 7, -1, 9, 9, 4, 1, -4, -2, 3, -3, -5, 4, -7, 7, 9, -4, 4, -8 ],-3)
