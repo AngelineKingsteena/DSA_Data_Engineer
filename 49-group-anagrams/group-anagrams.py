38f768081277f687a3c3ff21ee2b6119aace7e26
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-03 19:26:49 +0530

    Time: 91 ms (92.37%) | Memory: 19.5 MB (84.26%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+      # Hash Map
+        hash_map = {}
+        # Iterate over each string in given list
+        for s in strs:
+            # Hash function to get key
+            key = ''.join(sorted(s))
+            # CASE: If key is in hash map
+            if key in hash_map:
+                hash_map[key].append(s)
+            # CASE: If key is not in hash map
+            else:
+                hash_map[key] = [s]
+        return list(hash_map.values())  
+        
