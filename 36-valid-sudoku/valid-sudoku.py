4cab7d3153623e547569ccad560446f37aebf75d
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-04 02:58:42 +0530

    Time: 88 ms (99.09%) | Memory: 16.4 MB (13.16%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,5 +1,9 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """
+         store the indexs of the numbers in a dictionary in (x, y) format. Then for every number check for same row, same col and same box condition.
+This will require a single traversal. The same box condition can be checked using pos[0]//3 == x//3 and pos[1]//3 == y//3 since i//3 and j//3 give the box position.
+        """
         boardMap = collections.defaultdict(list)
         for x in range(9):
             for y in range(9):
