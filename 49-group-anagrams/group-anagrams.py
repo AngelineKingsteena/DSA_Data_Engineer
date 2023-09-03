f7b78504b45ec0caf4f798a3032c68e1edb69242
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-03 19:41:11 +0530

    Time: 87 ms (97.17%) | Memory: 19.8 MB (69.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -13,4 +13,12 @@ class Solution:
             else:
                 hash_map[key] = [s]
         return hash_map.values()
+
+# T.C
+
+# O(nlogn)
+
+# The time complexity  is O(n log(n)), where n is the length of the string.
+
+# The sorting algorithm used is the merge sort algorithm. The merge sort algorithm works by recursively dividing the array in half and then merging the two halves back together. The merge sort algorithm has a worst-case time complexity of O(n log(n)).
         
