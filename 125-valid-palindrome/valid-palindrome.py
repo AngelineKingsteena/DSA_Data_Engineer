4858abd00e7408d59ccdca6ab8e2e1563ba42a00
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-04 06:43:11 +0530

    Time: 40 ms (96.00%) | Memory: 18 MB (17.23%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,9 +3,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         l=s.lower()
         S=re.sub(r"[^a-z0-9]","",l)
-        if S=="":
-            return True
-        if (S==S[::-1]):
+        if S=="" or (S==S[::-1]):
             return True
         else:
             return False
