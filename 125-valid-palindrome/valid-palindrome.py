f7c5a9a6725372f49601121d6c8666f427350ba5
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-04 06:46:48 +0530

    Time: 46 ms (85.05%) | Memory: 17.9 MB (27.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -8,4 +8,9 @@ class Solution:
         else:
             return False
 
+
+##class Solution:
+    # def isPalindrome(self, s: str) -> bool:
+    # 	s = [i for i in s.lower() if i.isalnum()]
+    # 	return s == s[::-1]
         
