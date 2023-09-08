434c0954f095e2ee33b7b49197856f3af3a0fb86
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-09 02:59:03 +0530

    Time: 65 ms (67.52%) | Memory: 16.5 MB (25.02%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        ansList = ListNode()
+        curr = ansList
+        carry = 0
+        while(l1 or l2 or carry):
+            
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+            
+            c = a + b + carry
+            carry = 1 if c >= 10 else 0
+            digit = c % 10
+            
+            curr.next = ListNode(digit)
+            
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+            
+        return ansList.next
+            
+
+# while calculating carry, we know that largesr one digit number is 9 so 9 + 9 = 18 and if there is any carry then + 1 = 19 will be the largest sum that we will get. So carry will always be one and not more that one.
+
+        
