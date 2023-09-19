8c2664eb2ad42fa6ded0b806449d28de8deade71
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-20 04:44:21 +0530

    Time: 57 ms (31.42%) | Memory: 17.7 MB (29.08%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -4,68 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # https://docs.google.com/document/d/1l9UDlLZeP0mQxExqjepsKbgYQj4nrNGUsyDlKDA1jyg/edit#heading=h.a7d3sil9l2qs
-#         Let's reverse 1 -> 2 -> 3 (k = 2).
-# Before we start reversing, append dummy head first: dummy -> 1 -> 2 -> 3.
-# Instead of explaning with text, let me show visual flow of my code.
-# cnt = 0
-# head
-# dummy -> 1   -> 2 -> 3
-# prev     cur
-
-# cnt = 1
-# head
-# dummy <-> 1     2 -> 3
-#           prev  cur
-
-# cnt = 2
-# head
-# dummy <-> 1  <- 2    3
-#                 prev cur
-
-# cnt % k == 0, update head.
-#          head
-#           ┌----------↓
-# dummy     1  <- 2    3
-#           prev       cur
-#    └------------↑
-
-# Finally, first k nodes are reversed.
-#              head
-# dummy -> 2 -> 1    -> 3
-#               prev    cur
-
-# cnt = 3
-#              head
-# dummy -> 2 -> 1   <-> 3      (nil)
-#                       prev    cur
-
-# Now, cur is nil, but cnt % k is not 0. We have to revert remaining nodes with substracting cnt by 1 until cnt % 2 becomes 0.
-# cnt = 2
-#              head
-# dummy -> 2 -> 1    -> 3
-#               prev    cur
-
-# Done.
-
-        root = ListNode(next=head) # dummy
-        prev = head = root
-        cur = head.next
-        cnt = 0
-
-        while cur:
-            cur.next, prev, cur = prev, cur, cur.next
-            cnt += 1
-
-            if cnt % k == 0:
-                head.next.next, head.next, head = cur, prev, head.next
-                prev = head
-
-        while cnt % k:
-            prev.next, prev, cur = cur, prev.next, prev
-            cnt -= 1
-
-        return root.next
-
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:        
+        # Check if we need to reverse the group
+        curr = head
+        for _ in range(k):
+            if not curr: return head
+            curr = curr.next
+		        
+				
+        # Reverse the group (basic way to reverse linked list)
+        prev = None
+        curr = head
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
         
+		
+        # After reverse, we know that `head` is the tail of the group.
+		# And `curr` is the next pointer in original linked list order
+        head.next = self.reverseKGroup(curr, k)
+        return prev
