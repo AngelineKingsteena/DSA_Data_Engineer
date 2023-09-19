fbbcf4569e42772b16840c3b800b5b4e681da518
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-20 04:24:30 +0530

    Time: 86 ms (89.33%) | Memory: 20.9 MB (19.25%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -6,18 +6,26 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         ## video solution :- https://www.youtube.com/watch?v=RCuBc4Zl-oY&ab_channel=TimothyHChang
-
-
         h = []
         for i,l in enumerate(lists):
-            if l: heappush(h, (l.val, i))
+            if l: 
+                # print("22 ",h)
+                heappush(h, (l.val, i))
+                
         dummy = curr = ListNode(0)
+        # print("33 ",h)
         while h:
+            # print("44 ",h)
             val, i = heappop(h)
+            # print("55 ",h)
             curr.next = ListNode(val)
+            # print(f"66 {lists[i]} i {i}")
             if lists[i].next:
+                # print("77 ",h)
                 heappush(h, (lists[i].next.val, i))
+                # print("88 ",h)
                 lists[i] = lists[i].next
+                # print(f"99 {lists[i]} i {i}")
             curr = curr.next
-        return dummy .next
+        return dummy.next
                 
