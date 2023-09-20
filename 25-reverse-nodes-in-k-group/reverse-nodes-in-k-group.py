527fd60672e5d4f93320fb88d0b9eed42134adc9
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-20 16:47:54 +0530

    Time: 56 ms (35.60%) | Memory: 17.6 MB (29.08%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -5,23 +5,25 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:        
-        # Check if we need to reverse the group
+        #The first for loop runs for k iterations. Its purpose is to check if there are at least k nodes remaining in the list. If not, it returns the original head, as reversing is not possible.
         curr = head
         for _ in range(k):
             if not curr: return head
             curr = curr.next
-		        
-				
+		        	
         # Reverse the group (basic way to reverse linked list)
         prev = None
         curr = head
+
+        # Once the first group of k nodes is reversed, the prev variable points to the new head of the reversed group,`head` is the tail of the group and the curr variable points to the next group of nodes that haven't been processed yet.
+
         for _ in range(k):
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
         
-		
+        
         # After reverse, we know that `head` is the tail of the group.
 		# And `curr` is the next pointer in original linked list order
         head.next = self.reverseKGroup(curr, k)
