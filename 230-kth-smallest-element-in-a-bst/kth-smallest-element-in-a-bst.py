e11f015f09b1d64e01864f8fa7a33a33d0329d62
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-21 21:02:08 +0530

    Time: 53 ms (62.85%) | Memory: 20.3 MB (98.16%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,21 +5,41 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         ## video solution :- https://www.youtube.com/watch?v=5LUXSvjmGCw&ab_channel=NeetCode
         ## since its a bst,solution can be found by doing inorder traversal --> L,N,R
-        stack = []
-        trav = root
-        count = 0
-        while trav or stack:
-            if trav:
-                stack.append(trav)
-                trav = trav.left
-            else:
-                ## get the parent
-                u = stack.pop()
-                count += 1
-                if count == k:
-                    return u.val
-                ## and start doing inorder travsersal of parent
-                trav = u.right
+
+        # ## iterative
+        # stack = []
+        # trav = root
+        # count = 0
+        # while trav or stack:
+        #     if trav:
+        #         stack.append(trav)
+        #         trav = trav.left
+        #     else:
+        #         ## get the parent
+        #         u = stack.pop()
+        #         count += 1
+        #         if count == k:
+        #             return u.val
+        #         ## and start doing inorder travsersal of parent
+        #         trav = u.right
+
+    # recurive inorder
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        inOrder = []
+        def doInOrderDFS(node):
+            if not node:
+                return
+            doInOrderDFS(node.left)
+            inOrder.append(node.val)
+            doInOrderDFS(node.right)
+
+        doInOrderDFS(root)
+        return inOrder[k-1]
