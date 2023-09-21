a3ca3f045ad234b709a622883bbccb56a29a21b5
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-22 01:27:40 +0530

    Time: 45 ms (58.98%) | Memory: 17.1 MB (30.97%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -4,8 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ## video solution :- https://www.youtube.com/watch?v=6ZnyEApgFYg&ab_channel=NeetCode
         res = []
         q = collections. deque ()
         q. append (root)
@@ -21,4 +24,20 @@ class Solution:
             if level:
                 res. append(level)
         return res
-                
+
+# # Create the binary tree for testing
+# #         3
+# #        / \
+# #       9  20
+# #          / \
+# #         15  7
+# root = TreeNode(3)
+# root.left = TreeNode(9)
+# root.right = TreeNode(20)
+# root.right.left = TreeNode(15)
+# root.right.right = TreeNode(7)
+
+# # Test the levelOrder function
+# solution = Solution()
+# result = solution.levelOrder(root)
+# print(result)  # Expected output: [[3], [9, 20], [15, 7]]
