808a149bbf9e76eea97ff196aac405b57d5b09b7
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-23 16:38:32 +0530

    Time: 90 ms (32.05%) | Memory: 16.4 MB (74.53%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -20,4 +20,28 @@ class Solution:
             dfs(i + 1, cur, total)
         dfs(0, [], 0)
         return res
+
+# IF YOU WANT OUTPUT WITH DUPLICATES LIKE BELOW:-
+# OUTPUT:-
+# [[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,2,2],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,2,2],[1,1,1,5],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,2,2],[1,1,1,5],[1,1,2,2,2],[1,1,6],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,2,2],[1,1,1,5],[1,1,2,2,2],[1,1,6],[1,2,5],[1,7],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],[1,1,1,1,2,2],[1,1,1,5],[1,1,2,2,2],[1,1,6],[1,2,5],[1,7],[2,2,2,2],[2,6]]
+
+# EXPECTED:- [[1,1,6],[1,2,5],[1,7],[2,6]]
+
+#     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = []
+#         candidates.sort()  # Sort the candidates to handle duplicates
+#         def dfs(i, cur, total):
+#             if total == target:
+#                 res.append(cur.copy())
+#                 return
+#             if i >= len(candidates) or total > target:
+#                 return
+#             # Include the current candidate
+#             cur.append(candidates[i])
+#             dfs(i, cur, total + candidates[i])
+#             cur.pop()
+#             # Skip the current candidate
+#             dfs(i + 1, cur, total)
+#         dfs(0, [], 0)
+#         return res
         
