f86be666a9c8be7805aa949e9290604195f0dddc
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-23 16:33:13 +0530

    Time: 82 ms (43.67%) | Memory: 16.2 MB (93.89%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,31 +1,27 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        # Sort the candidates to handle duplicates
-        candidates.sort()  
+        candidates.sort()  # Sort the candidates to handle duplicates
+        
         def dfs(i, cur, total):
             if total == target:
                 res.append(cur.copy())
                 return
-            for j in range(i, len(candidates)):
-                if j > i and candidates[j] == candidates[j-1]:
-                    # Skip duplicates to avoid duplicates 
-                    # in the result
-                    continue  
-                if total + candidates[j] <= target:
-                    cur.append(candidates[j])
-                    # Use j + 1 to avoid reusing the same number
-                    # WITHOUT J+1
-                    # candidates =[10,1,2,7,6,1,5]
-                    # target =8
-                    # Output
-                    # [[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,2],
-                    # [1,1,1,1,2,2],[1,1,1,5],[1,1,2,2,2],[1,1,6],
-                    # [1,2,5],[1,7],[2,2,2,2],[2,6]]
-                    # Expected
-                    # [[1,1,6],[1,2,5],[1,7],[2,6]]
-                    dfs(j+1, cur, total + candidates[j])
-                    cur.pop()
+            if i >= len(candidates) or total > target:
+                return
+            
+            # Include the current candidate
+            cur.append(candidates[i])
+            dfs(i + 1, cur, total + candidates[i])
+            cur.pop()
+            
+            # Skip duplicates
+            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+                i += 1
+            
+            # Skip the current candidate
+            dfs(i + 1, cur, total)
+        
         dfs(0, [], 0)
         return res
         
