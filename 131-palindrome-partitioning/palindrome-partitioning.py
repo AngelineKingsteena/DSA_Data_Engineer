32b0f9795869e943b4cbd7cce5a9681002cf0077
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-23 21:17:19 +0530

    Time: 561 ms (38.15%) | Memory: 33.7 MB (45.39%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -3,15 +3,13 @@ class Solution:
         ## video solution :- https://www.youtube.com/watch?v=xhEEpnn0x1U&ab_channel=TimothyHChang
         ## s.c :-
         ## t.c :-2^n
-
-
-        n= len(s)
+        # n= len(s)
         output = []
         def dfs(st,sofar):
             #base case
-            if st==n:
+            if st==len(s):
                 output.append(sofar)
-            for i in range(st,n):
+            for i in range(st,len(s)):
                 if s[st:i+1]==s[st:i+1][::-1]: #is palindrome?
                     dfs(i+1, sofar+[s[st:i+1]])
         dfs(0, [])
