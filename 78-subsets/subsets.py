9ecd8bda087d29a7b132f8978cf0472429593016
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-23 15:44:24 +0530

    Time: 47 ms (19.95%) | Memory: 16.4 MB (92.62%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,20 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ## video solution :- https://www.youtube.com/watch?v=REOH22Xwdkk&ab_channel=NeetCode
-        # T.C :- O(n.2^n),the 2^n is the no. of sub-sets posssible
-        # n is maximum digits one or more of the subsets can have
-        res = []
-        subset = [ ]
-        def dfs(i):
-            if i == len (nums):
-                res.append(subset.copy ())
-                return
-            # decision to include nums [i]
-            subset.append (nums [ i])
-            dfs(i + 1)
-            # decision NOT to include nums[i]
-            subset .pop ( )
-            dfs(i + 1)
-        dfs (0)
-        return res
-        
+        def backtrack(start, end, tmp):
+            ans.append(tmp[:])
+            for i in range(start, end):
+                tmp.append(nums[i])
+                backtrack(i+1, end, tmp)
+                tmp.pop()
+        ans = []
+        backtrack(0, len(nums), [])
+        return ans
+            
