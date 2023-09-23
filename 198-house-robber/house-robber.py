91c7657f0fbb860603c7a527a6c6d9f9cc28b6a8
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-24 03:43:01 +0530

    Time: 41 ms (46.94%) | Memory: 16.2 MB (89.92%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        ## https://leetcode.com/problems/house-robber/solutions/1605193/with-explanation-dp-with-o-l-time-and-o-1-space-in-python/
+        #edge case
+        if len(nums) <= 2:
+            return max(nums)
+        #dp (less space)
+        a, b = nums[0], max(nums[0], nums[1])
+        for i in range(2,len(nums)):
+            a, b = b, max(a + nums[i], b)
+        return b
+        
