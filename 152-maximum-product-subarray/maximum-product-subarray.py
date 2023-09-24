60e5276428ac5a2cae3cc3415f0ed41e3ec42753
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-24 14:05:05 +0530

    Time: 86 ms (25.84%) | Memory: 16.8 MB (53.16%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -2,12 +2,14 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         curMax, curMin = 1, 1
         res = nums[0]
-        
         for n in nums:
             vals = (n, n * curMax, n * curMin)
             curMax, curMin = max(vals), min(vals)
-			
             res = max(res, curMax)
-            
         return res
+
+# Subproblem for the DP here would be: What is the maximum and minimum product we can get for a contiguous sub-array starting from the 0th to the current element? Why do we need to maintain the minimum product while we are asked for a maximum? The fact is that elements in nums can be negative, so it possible that for some negative element the previous min possible product can turn the current product into a greater value.
+
+# Time: O(n) - scan
+# Space: O(1)
         
