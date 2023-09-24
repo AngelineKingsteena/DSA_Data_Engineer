7f9895a505426031d3321591b4751af9df0c65a9
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-24 13:48:59 +0530

    Time: 978 ms (57.95%) | Memory: 16.6 MB (90.43%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,10 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount + 1] * (amount + 1)
-        dp [0] = 0
+        dp [0] = 0 ## 0 WAYS TO MAKE AMOUNT 0
         for a in range (1, amount + 1):
             for c in coins:
-                if a - c >= 0:
+                if a - c >= 0:## IF IT ENDS UP NEGATIVE,NO SOLUTION
                     dp[a] = min(dp[a], 1 + dp[a - c])
+                    # COIN=4
+                    # A=7
+                    # DP[7]=1+DP[7-4]
         return dp [amount] if dp [amount] != amount + 1 else -1
         
