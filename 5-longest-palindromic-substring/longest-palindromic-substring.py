59bba06b106a0c22277f451f542f4a45839a1e3f
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-24 06:21:18 +0530

    Time: 397 ms (77.82%) | Memory: 16.4 MB (67.28%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,9 +1,13 @@
 class Solution:
 	def longestPalindrome(self, s: str) -> str:
+		# it starts searching for a palindrome from a single character position. It essentially treats the character at index as the center of a potential odd-length palindrome.
+
+# 		when helper(index, index) is called, it starts expanding from the single character at position index as the potential center of an odd-length palindrome.
+
+# Similarly, when helper(index, index + 1) is called, it starts expanding from the two adjacent characters at positions index and index + 1 as the potential center of an even-length palindrome.
 		res = ""
-		length = len(s)
 		def helper(left: int, right: int):
-			while left >= 0 and right < length and s[left]==s[right]:
+			while left >= 0 and right <len(s) and s[left]==s[right]:
 				left -= 1
 				right += 1				
 			return s[left + 1 : right]
