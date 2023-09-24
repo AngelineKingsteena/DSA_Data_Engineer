f12802f9953393aae0a07fd283da55a255348829
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-24 06:46:41 +0530

    Time: 42 ms (46.05%) | Memory: 16.2 MB (78.02%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,14 +1,31 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         # https://leetcode.com/problems/decode-ways/solutions/1029225/simple-and-detail-solution-with-explanation/
-        dp = [0] * (len(s) + 1)
-        dp[0],dp[1]=1,1
-        if s[0] == "0":
+        # cannot map to any character due to the leading zero
+        if s[0] == '0':
             return 0
-        for i in range(2, len(s) + 1):
-            if 1<=int(s[i-1])<=9:
-                dp[i]+= dp[i - 1]
-            if 10 <= int(s[i - 2] + s[i - 1]) <= 26 :
-                dp[i]+=dp[i - 2] 
-        return dp[-1]
+        n = len(s)
+        # dp[i]: number of ways of decoding the substring s[:i]
+        dp = [0 for _ in range(n + 1)]
+        # base case
+        dp[0] = 1
+        for i in range(1, n + 1):
+            # check single digit decode
+            # valid deocde is possible only when s[i - 1] is not zero
+            # if so, take the previous state dp[i - 1]
+            # e.g. AB - 1[2]
+            if s[i - 1] != '0':
+                dp[i] = dp[i - 1]
+            # check double digit decode
+            # by looking at the previous two digits
+            # if the substring belongs to the range [10 - 26]
+            # then add the previous state dp[i - 2]
+            # e.g. L - [12]
+            if i >= 2:
+                # or you can use `stoi(s.substr(i - 2, 2))`
+                x = int(s[i - 2: i])
+                # check the range
+                if 10 <= x <= 26:
+                    dp[i] += dp[i - 2]
+        return dp[n]
         
