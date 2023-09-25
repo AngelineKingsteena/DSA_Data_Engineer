3d17a554748fc7cf94e0ff520a9614df790c4f0b
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-25 20:26:20 +0530

    Time: 48 ms (32.02%) | Memory: 16.6 MB (97.90%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self,node:Optional['Node'])->Optional['Node']:
+# https://www.youtube.com/watch?v=mQeF6bN8hMk&ab_channel=NeetCode
+        oldToNew={}
+        def clone_recursive(node):
+            if node in oldToNew:
+                return oldToNew [node]
+            copy = Node (node.val)
+            #map original node to it's copy
+            oldToNew[node] = copy
+            #make copies of every neighbors of original node
+            for nei in node.neighbors:
+                copy.neighbors.append(clone_recursive(nei))
+            return copy
+        return clone_recursive(node) if node else None
+
+        
