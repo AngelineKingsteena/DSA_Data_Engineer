fc839349ccd42b52ea5bf5d2137c3fb8dfac1c99
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-27 20:06:29 +0530

    Time: 42 ms (32.52%) | Memory: 16.2 MB (92.71%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # https://www.youtube.com/watch?v=IlEsdxuD4lY&ab_channel=NeetCode
+
+    # idea:- replicate below (notice last row ,last column is 1)
+        # 28,21,15,10,6,3,1
+        #  7, 6, 5, 4,3,2,1 (2=right+down ,3=right+down)
+        #  1, 1, 1, 1,1,1,1
+
+        #act as last row
+        row = [1] * n
+        # all rows except last one
+        for i in range(m - 1):
+            #after 1st full iteration #  1, 1, 1, 1,1,1,1
+            #after 2nd full iteration #  7, 6, 5, 4,3,2,1
+            newRow = [1] * n
+            #to do right,leave last column that's why n-2
+            for j in range(n - 2, -1, -1):
+                #after 1st full iteration #  7, 6, 5, 4,3,2,1
+                #after 2nd full iteration # 28,21,15,10,6,3,1
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
+        
