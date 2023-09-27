0eeed2448fd37926c72791cbb8181f204b649f9c
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-27 20:51:35 +0530

    Time: 613 ms (58.50%) | Memory: 42.4 MB (45.42%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -5,7 +5,9 @@ class Solution:
         for i in range(len(text1) - 1, -1, -1):
             for j in range(len (text2) - 1, -1, -1):
                 if text1[i] == text2[j]:
+                    ## diagonal
                     dp[i][j] = 1 + dp[i + 1][j + 1]
                 else:
+                    ## max(right,bottom)
                     dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
         return dp[0][0]
