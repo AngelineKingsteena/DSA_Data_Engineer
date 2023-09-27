149a45e42d8ee7b1aed2a919645b9f225d93911c
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-28 02:37:33 +0530

    Time: 41 ms (41.04%) | Memory: 16 MB (99.95%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,33 +1,26 @@
 class Solution:
-#     def uniquePaths(self, m: int, n: int) -> int:
-#         # https://www.youtube.com/watch?v=IlEsdxuD4lY&ab_channel=NeetCode
-
-# # idea:- replicate below (notice last row ,last column is fully 1)
-#         # 28,21,15,10,6,3,1
-#         #  7, 6, 5, 4,3,2,1 (2=right+down ,3=right+down)
-#         #  1, 1, 1, 1,1,1,1
-#         # start location is 1,end is 28
-
-#         #act as last row
-#         row = [1] * n
-#         # all rows except last one
-#         for i in range(m - 1):
-#             #after 1st full iteration #  1, 1, 1, 1,1,1,1
-#             #after 2nd full iteration #  7, 6, 5, 4,3,2,1
-#             newRow = [1] * n
-#             #to do right,leave last column that's why n-2
-#             for j in range(n - 2, -1, -1):
-#                 #after 1st full iteration #  7, 6, 5, 4,3,2,1
-#                 #after 2nd full iteration # 28,21,15,10,6,3,1
-#                 newRow[j] = newRow[j + 1] + row[j]
-#             row = newRow
-#         return row[0]
-        
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1] * n for _ in range(m)]
+        # https://www.youtube.com/watch?v=IlEsdxuD4lY&ab_channel=NeetCode
 
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+# idea:- replicate below (notice last row ,last column is fully 1)
+        # 28,21,15,10,6,3,1
+        #  7, 6, 5, 4,3,2,1 (2=right+down ,3=right+down)
+        #  1, 1, 1, 1,1,1,1
+        # start location is 1,end is 28
 
-        return dp[m - 1][n - 1]
+        #act as last row with n columns
+        row = [1] * n
+        # all rows except last one
+        for i in range(m - 1):
+            #after 1st full iteration #  1, 1, 1, 1,1,1,1
+            #after 2nd full iteration #  7, 6, 5, 4,3,2,1
+            newRow = [1] * n
+            #to do right,leave last column that's why n-2
+            for j in range(n - 2, -1, -1):
+                #after 1st full iteration #  7, 6, 5, 4,3,2,1
+                #after 2nd full iteration # 28,21,15,10,6,3,1
+                #newRow[j + 1]=right ,row[j]=down
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
+        
