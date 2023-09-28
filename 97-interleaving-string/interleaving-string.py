41226ab37c98f401f7badbafd66a22113c3516f7
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-28 05:59:01 +0530

    Time: 53 ms (31.31%) | Memory: 16.3 MB (96.36%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -8,8 +8,10 @@ class Solution:
         for i in range(len (s1), -1, -1):
             for j in range (len (s2), -1,-1):
                 ##11:25 timestamp on video
+                ## look in the bottom
                 if i<len(s1) and s1[i] == s3[i+j] and dp[i + 1][j]:
                     dp[i][j] = True
+                ## look in the right
                 if j<len(s2) and s2[j] == s3[i+j] and dp[i][j + 1]:
                     dp[i][j] = True
         return dp[0][0]
