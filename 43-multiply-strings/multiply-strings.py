bd1a64732c63c3800f704f22ab74530c046bb51e
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-29 04:49:51 +0530

    Time: 35 ms (90.26%) | Memory: 16.2 MB (90.32%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,8 +1,10 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         def strint(n):
+            ##convert num1 and num2 strings into int
             result=0
             for i in range(len(n)):
+                ## convert"22" into int by 2*10+2
                 result = result*10 + ord(n[i])-ord('0')
             return result
         return str(strint(num1)*strint(num2))
