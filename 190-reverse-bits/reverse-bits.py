228cece07b42fad1c807284e03e27abd80a1e05b
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-29 16:53:59 +0530

    Time: 43 ms (39.32%) | Memory: 16.1 MB (96.51%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -3,6 +3,8 @@ class Solution:
         # https://www.youtube.com/watch?v=UcoN6UjAI64&ab_channel=NeetCode
         res=0
         for i in range (32):
+            # take each bit from right
             bit = (n >> i) & 1
+            # add it to extreme left bit in total 32 bit
             res = res | (bit << (31 - i))
         return res
