8d02cfd1f17416f75122f738962ed1b2b49f6c79
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-30 08:28:57 +0530

    Time: 42 ms (58.97%) | Memory: 16.2 MB (85.65%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -2,6 +2,7 @@ class Solution(object):
     def isHappy(self, n):
         hset = set()
         while n != 1:
+            ## TO AVOID CCYCLE --> 2 will eventaully become 37 and then 61-->it'll again become 37
             if n in hset: return False
             hset.add(n)
             n = sum([int(i) ** 2 for i in str(n)])
