5cf93e77c5a6d6a0e922c0df5429699202e32386
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-09-30 09:48:16 +0530

    Time: 39 ms (47.11%) | Memory: 16.1 MB (86.07%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -32,7 +32,8 @@ class Solution:
     def getSum(self, a: int, b: int) -> int:
         mask = 0xffffffff
         while(b & mask > 0):
-            carry = (a & b) << 1
-            a = a ^ b
-            b = carry
+            # carry = (a & b) << 1
+            # a = a ^ b
+            # b = carry
+            a,b=a ^ b,(a & b) << 1
         return (a & mask) if b > 0 else a
