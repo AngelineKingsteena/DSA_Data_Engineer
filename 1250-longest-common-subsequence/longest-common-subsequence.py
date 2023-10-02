fa5bf0b209bfd6d4b8da9a00227b6b10032b8124
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-03 04:19:42 +0530

    Time: 587 ms (75.38%) | Memory: 42.2 MB (56.11%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,6 +1,27 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         # https://www.youtube.com/watch?v=Ua0GhsJSlWM&ab_channel=NeetCode
+
+        ##idea:-
+        # create a 2 d matrix of text1 & text2 with extra row and extra column and if the letters match,then add 1 with diigonal value,else move  right or botom and take max of either
+
+        ## logic
+    #         a  b  ""
+    #       ----------
+    #     a |  |  | 0
+    #     c |  |  | 0
+    #     b |  |1 | 0 ## here 0,coz if we match "" with "",0 lcs; 1 coz if match "b" with "b"
+    #    " "|0 | 0| 0
+
+    ## max(right,bottom) when mismatch,coz in below,if u go down lcs=2,but on right lcs=1
+        # c  e  ""
+#       ----------
+#     c |  |  | 0
+#     d |  |  | 0
+#     e |  |1 | 0 
+#    " "|0 | 0| 0
+
+# range(len(text2) + 1) becoz that extra 0
         dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
         for i in range(len(text1) - 1, -1, -1):
             for j in range(len (text2) - 1, -1, -1):
