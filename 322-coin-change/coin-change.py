803c6c272843ec08f33e94a383d56880e663e29d
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-03 16:25:54 +0530

    Time: 973 ms (59.41%) | Memory: 16.6 MB (69.35%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,14 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         #https://www.youtube.com/watch?v=H9bfqozjoqs&ab_channel=NeetCode
+        # T.C:- O(amount* len(coins))
+        # set default values
         dp = [amount + 1] * (amount + 1)
         dp [0] = 0 ## 0 WAYS TO MAKE AMOUNT 0
-        for a in range (1, amount + 1):
-            for c in coins:
+        for c in coins:
+            for a in range (1, amount + 1):
+            
                 if a - c >= 0:## IF IT ENDS UP NEGATIVE,NO SOLUTION
                     dp[a] = min(dp[a], 1 + dp[a - c])
                     # COIN=4
                     # A=7
                     # DP[7]=1+DP[7-4]
+        # return non-default values
         return dp [amount] if dp [amount] != amount + 1 else -1
         
