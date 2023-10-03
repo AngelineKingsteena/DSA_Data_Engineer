efbffb18a7bf9989b7973d43f5607e990c1c785d
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-03 19:18:40 +0530

    Time: 1642 ms (31.53%) | Memory: 22.2 MB (45.44%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,16 +1,28 @@
 class Solution:
 # This is a classic knapsack problem.
-# Whenever you have an array, and you pretty much want to partition it based upon some criteria, you can use a similar format as shown below.
+# Whenever you have an array, and you want to partition it based upon some criteria as shown below.
 # Change the thinking from:
 # "separating elements into two arrays, then calculating the difference of their sums"
 # To the following:
 # Whenever you partition two elements into different arrays/groups, your basically subtracting them from each other. If you put them in the same group, you're adding them. Thus, you want to try every possible combination of adding and subtracting them from eachother to see if you can reach zero!
+
 # {num - i for i in dp} is pretty much subtracting every element in dp from the current number
 # {num + i for i in dp} is pretty much adding every element in dp to the current number
 # | does a union between these two sets.
     def canPartition(self, nums: List[int]) -> bool:
-        dp = {0}
+        #it's always possible to create an empty subset with a sum of 0.
+        # Initialize a set 'possible_sums' with 0 to represent an empty subset with a sum of 0.
+        possible_sums = {0}
+        # Iterate through each 'num' in the input list 'nums'.
         for num in nums:
-            dp = {num - i for i in dp} | {num + i for i in dp} 
-        return 0 in dp
-        
+            # Create two sets:
+            # 1. Subtract 'num' from each element in 'possible_sums'.
+            subtracted_sums = {sum - num for sum in possible_sums}
+            # 2. Add 'num' to each element in 'possible_sums'.
+            added_sums = {sum + num for sum in possible_sums}
+            # Combine the two sets using union (|) to update 'possible_sums' with new sums.
+            possible_sums = subtracted_sums | added_sums
+        # Check if 0 is in 'possible_sums', which means it's possible to partition the list into
+        # two subsets with equal sums.
+        return 0 in possible_sums
+            
