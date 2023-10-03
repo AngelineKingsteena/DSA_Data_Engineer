c17110be31f0452eb249e1c0e8447bdcaf9c3c2f
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-03 11:22:08 +0530

    Time: 45 ms (9.16%) | Memory: 16.3 MB (54.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,8 +2,12 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         ## video solution :- https://www.youtube.com/watch?v=Y0lT9Fck7qI&ab_channel=NeetCode
         ## s.c :-
-        ## t.c :-
-        one, two = 1, 1
+        ## t.c :-n
+        #[newOne,newTwo/oldOne,oldTwo]
+        #[1+1,1,1]
+        one, two = 1, 1 #two is no. of steps to reach top from top
+         #two is no. of steps to reach top from top before step
+        ## skip last two
         for i in range(n - 1):
             temp = one
             one = one + two
