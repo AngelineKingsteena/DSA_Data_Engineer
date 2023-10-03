9ee06e06d626a596728029c2ae843efadbb2725c
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-03 13:51:48 +0530

    Time: 59 ms (68.56%) | Memory: 16.4 MB (56.17%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -8,4 +8,4 @@ class Solution:
         cost.append(0)
         for i in range(len (cost) - 3, -1, -1):
             cost[i] += min (cost[i + 1], cost[i + 2])
-        return min(cost [0], cost [1]) #because You can either start from the step with index 0, or the step with index 1.
+        return min(cost [0], cost [1]) #because question says:-You can either start from the step with index 0, or the step with index 1.
