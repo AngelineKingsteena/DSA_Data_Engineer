ad9debba28533655a3b6ca928ea332a9451b8c60
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-03 05:45:24 +0530

    Time: 116 ms (79.57%) | Memory: 16.5 MB (82.66%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -16,17 +16,19 @@ class Solution:
 # Initialize dp as [1, 0, 0, 0, 0, 0] (size amount + 1).
 ## each element in dp represents an no.of ways to make index amount 
 # [N for re.0,N for rs.1,N for re.2..etc]..where N=no.of ways
+
+
 # For each coin denomination (1, 2, and 5):
 # When considering the coin with value 1, update dp from left to right:
 # dp[1] += dp[0]: dp becomes [1, 1, 0, 0, 0, 0]
 # dp[2] += dp[1]: dp becomes [1, 1, 1, 0, 0, 0]
 # ...
-# dp[5] += dp[4]: dp becomes [1, 1, 1, 1, 1, 1]##  
+# dp[5] += dp[4]: dp becomes [1, 1, 1, 1, 1, 1]##no of ways 1re is used  
 # When considering the coin with value 2, update dp from left to right:
 # dp[2] += dp[0]: dp becomes [1, 1, 2, 1, 1, 1]
 # dp[3] += dp[1]: dp becomes [1, 1, 2, 2, 1, 1]
 # ...
-# dp[5] += dp[3]: dp becomes [1, 1, 2, 2, 3, 3]
+# dp[5] += dp[3]: dp becomes [1, 1, 2, 2, 3, 3]##no of ways both 1rs and 2rs is used  
 # When considering the coin with value 5, update dp from left to right:
-# dp[5] += dp[0]: dp becomes [1, 1, 2, 2, 3, 4]
+# dp[5] += dp[0]: dp becomes [1, 1, 2, 2, 3, 4]##no of ways all 1rs,2rs,5rs are used  
 # The final result is dp[5], which is 4. This means there are 4 different combinations to make up 5 using the coins [1, 2, 5].
