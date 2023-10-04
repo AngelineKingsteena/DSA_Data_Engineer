2e362a337ac7d7bb55d8525cd65beb5665dcd660
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-05 04:42:07 +0530

    Time: 78 ms (81.44%) | Memory: 19.8 MB (67.24%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -3,22 +3,20 @@ class Solution:
 # https://www.youtube.com/watch?v=A8NUOmlwOlM&ab_channel=NeetCode
         res = []
         for i in range(len (intervals)):
-            ## new intervals end< current ith interval's start
-            ## then add new interval
+            ## new intervals ends before rest of the intervals
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
-            ## new intervals start> current ith interval's end
-            ## then add ith interval
+            ## new intervals starts after current interval ends
             elif newInterval[0] > intervals[i][1]:
                 res.append (intervals[i])
             else:
                 ##if new interval is overlapping,loop thru all 
                 # ith inteval,and take extremeties from 
                 # all i intevals and create custom new interval,
-                ##BUT DONT ADD IT YET,CAUSE KEEP LOOPING 
+                ##BUT DONT ADD IT YET,KEEP LOOPING 
                 # UNTIL ALL iTH INTERVAL'S ENDS FALL UNDER
-                # NEW INTERVAL'S END
+                # custom INTERVAL'S END
                 newInterval = [min(newInterval[0], intervals [i][0]), max(newInterval [1], intervals [i][1])]
         res.append (newInterval)
         return res
