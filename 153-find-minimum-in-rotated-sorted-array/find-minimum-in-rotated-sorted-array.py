0b2cb93c1dbdd181ed7c21897f4306dc8cb7cee0
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-06 17:39:51 +0530

    Time: 51 ms (26.71%) | Memory: 16.6 MB (34.73%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,24 +1,23 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        ## video solution :-https://www.youtube.com/watch?v=nIVW4P8b1VA&ab_channel=NeetCode
-        res = nums [0]
-        l, r = 0, len (nums) - 1
+        l, r = 0, len(nums) - 1
+        
         while l <= r:
-            # if already in proper ascending order,obviously the leftmost is the minimum,so we can break out of algo
-            if nums [l] < nums[r]:
-                res = min(res, nums [l])
-                break
-            m = (l + r) // 2
-            ## example in [3,1,2],first iteration,l=0,r=2,nums [m]=1,end of iteration nums [m]=3
-            res = min(res, nums [m])
+            # if already in proper ascending order,obviously the leftmost is the minimum
+            if nums[l] <= nums[r]:
+                return nums[l]
+            mid = (l + r) // 2
+            # Check if the minimum element is in the left or right sorted portion
             #  checks whether the subarray from l to m is sorted,then right might have min,cause
-            # | /|      in [4,5,6,7,0,1,2] (right has the min)
-            # |/ | / 
-            #    |/
-            if nums [l] <= nums [m]:
-                l = m + 1
+                # | /|      in [4,5,6,7,0,1,2] (right has the min)
+                # |/ | / 
+                #    |/
+            if nums[mid] >= nums[l]:
+                # Minimum is in the right sorted portion
+                l = mid + 1
             # if not sorted,min might be on the other side i,.e right side
             else:
-                r = m - 1
-        return res
-        
+                # Minimum is in the left sorted portion
+                r = mid    
+        # The loop should always find the minimum, so this line is unlikely to be executed.
+        return -1
