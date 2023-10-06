c84093ba38d764b86dd6aa2e500b7f2e9328dddc
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-06 16:07:33 +0530

    Time: 49 ms (44.89%) | Memory: 16.6 MB (67.44%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -3,15 +3,19 @@ class Solution:
         ## video solution :-https://www.youtube.com/watch?v=nIVW4P8b1VA&ab_channel=NeetCode
         left, right = 0, len(nums) - 1
     
-        while left < right:
-            mid = (left + right )// 2
-            
-            # Check if the mid element is greater than the rightmost element
+        while left <= right:
+            if nums[left] <= nums[right]:
+                # If the leftmost element is less than or equal to the rightmost element,
+                # it means the array is already sorted, and the minimum is at the 'left' index.
+                return nums[left]
+
+            mid = left + (right - left) // 2
+
             if nums[mid] > nums[right]:
                 left = mid + 1
             else:
                 right = mid
-        
+
         # The minimum element will be at the 'left' index
         return nums[left]
-            
+                
