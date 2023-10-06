488b37308bafc446a26999dea814078ac04eb949
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-06 11:16:32 +0530

    Time: 104 ms (55.95%) | Memory: 16.3 MB (62.29%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,6 +1,10 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        ## video solution :- https://youtu.be/gqXU1UyA8pk?feature=shared
+        ## video solution :- https://www.youtube.com/watch?v=gqXU1UyA8pk&ab_channel=NeetCode
+        ### IDEA: we need to get longest repeating string,
+        # so the logic we'll be using is :- length of sring sofar-max count of char/Count of repeating char sofar> k 
+        # (r - l + 1) - maxf <= k
+        # if not so move the left ,move left pointer
         count = {}
         res= 0
         l = 0
@@ -9,6 +13,7 @@ class Solution:
             count [s[r]] = 1 + count.get(s[r], 0)
             maxf = max(maxf, count[s[r]])
             while (r - l + 1) - maxf > k:
+                # we want to move left pointer ahead,so count of char at l,should be considered for reduction
                 count[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1)
