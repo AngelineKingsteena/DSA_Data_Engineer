538458f065dfa0cb30732493cef11d336fcf6999
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-06 17:42:10 +0530

    Time: 44 ms (75.23%) | Memory: 16.4 MB (91.98%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -18,4 +18,8 @@ class Solution:
             # if not sorted,min might be on the other side i,.e right side
             else:
                 # Minimum is in the left sorted portion
-                r = mid    
+                r = mid  
+
+# why r=mid instead of r=mid-1?
+# If nums[mid] >= nums[l], setting r = mid - 1 would exclude the possibility of nums[mid] being the minimum element. This is because the minimum element is more likely to be in the right portion, and mid itself, could potentially be the minimum.
+# r = mid allows the binary search to continue examining the rotated portion that contains mid as a potential minimum.  
