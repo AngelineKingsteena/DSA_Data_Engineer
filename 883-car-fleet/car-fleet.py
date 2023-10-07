d275ac03bb0feeed0455d92956f1edd47aa34101
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-10-07 11:50:32 +0530

    Time: 875 ms (11.01%) | Memory: 36.2 MB (84.36%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -3,8 +3,9 @@ class Solution:
         ## video solution :-https://www.youtube.com/watch?v=Pr6T-3yB9RM&ab_channel=NeetCode
         pair = [[p, s] for p, s in zip(position, speed)]
         stack = []
+        # sorted and reversed coz, the car closeest to destination will anyway block the rest of the cars behind it to maybe form a fleet,if they catch up
         for p, s in sorted(pair)[::-1]: # Reverse Sorted Order
-            stack.append((target - p) / s)
+            stack.append((target - p) / s) # time taken
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 ### remove the lesser time car
                 stack.pop()
