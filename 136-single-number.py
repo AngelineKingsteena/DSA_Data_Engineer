0f3bed8405ad1be7e2abbcead16048efa50442c6
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-04 11:05:11 +0530

    Delete 136-single-number.py

--- 136-single-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-# https://www.youtube.com/watch?v=qMPX1AOa83k&ab_channel=NeetCode
-        res = 0
-        for n in nums:
-            ## because a(xor)a=0 ,but a(xor)0=a
-            res = n ^ res
-        return res
-        
