097a50ee8019b2d8370830f4b3cfb470b6ec0baa
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-05 06:03:51 +0530

    Update gas-station.py

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -5,13 +5,13 @@ class Solution:
         if sum(gas) < sum(cost):
             return -1
         total = 0
-        res= 0
+        start= 0
         for i in range(len (gas)):
             total += (gas[i] - cost[i])
             if total < 0:
                 total=0
-                res=i+1
-        return res
+                start=i+1
+        return start
 
 
         
