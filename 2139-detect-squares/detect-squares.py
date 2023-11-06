20ffb9a1f19a3b3670c95aea10ad116c6a078659
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-06 12:57:19 +0530

    Update detect-squares.py

--- 2139-detect-squares/detect-squares.py
+++ 2139-detect-squares/detect-squares.py
@@ -1,20 +1,21 @@
 class DetectSquares:
 # https://www.youtube.com/watch?v=bahebearrDc&ab_channel=NeetCode
-    def __init__ (self):
-        self.ptsCount = defaultdict (int)
-        self.pts = []
+    def __init__(self):
+        self.pts = defaultdict(int)
 
     def add(self, point: List[int]) -> None:
-        self.ptsCount[tuple(point)] += 1
-        self.pts.append(point)
+        self.pts[tuple(point)] += 1
 
     def count(self, point: List[int]) -> int:
-        res=0
+        res = 0
         px, py = point
+        #print('New')
         for x, y in self.pts:
-            if (abs (py - y) != abs(px - x)) or x == px or y == py:
+            if (abs(px - x) != abs(py - y)) or x == px or y == py:
                 continue
-            res += self.ptsCount[(x, py)] * self.ptsCount [ (px,y)]
+            if (px, y) in self.pts and (x, py) in self.pts:
+                # Multiply with the number of occurrences of the repeated point represented by self.pts[(x, y)]
+                res += self.pts[(px, y)] * self.pts[(x, py)] * self.pts[(x, y)]
         return res
         
 
