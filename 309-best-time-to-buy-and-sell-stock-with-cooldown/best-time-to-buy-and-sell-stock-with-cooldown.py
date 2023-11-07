e6445d0a2118563b0dc8d04872e47833afa104a0
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-07 06:23:09 +0530

    Update best-time-to-buy-and-sell-stock-with-cooldown.py

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,20 +2,20 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 # https://www.youtube.com/watch?v=I7j0F7AHpb8&ab_channel=NeetCode
         # State: Buying or Selling?
-        # If Buy -> 1 + 1
-        # If Sell -› 1 + 2
+        # If Buy -> i + 1
+        # If Sell -› i + 2 coz after selling there's a compulsory cooldown
         dp = {} # key=(i, buying) val=max_profit
         def dfs(i, buying) :
             if i >= len (prices):
                 return 0
             if (i, buying) in dp:
-                return dp[(i, buying)]
+                return dp[(i, buying)]#buying is only available 
             cooldown = dfs(i + 1, buying)
             if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
+                buy = dfs(i + 1, not buying) - prices[i]#not buying,coz you can only either sell or cool down
                 dp[ (i, buying)] = max (buy, cooldown)
             else:
-                sell = dfs(i + 2, not buying) + prices [i]
+                sell = dfs(i + 2, not buying) + prices [i]#not buying,coz you can cool down
                 dp[(i, buying)] = max (sell, cooldown)
             return dp[(i, buying)]
         return dfs(0, True)
