836c982a791d20ff1c322d147f7a30226ca83c73
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-10 05:21:45 +0530

    Update palindromic-substrings.py

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -2,14 +2,15 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
         for i in range(len (s)):
-            res += self.countPali(s, i, i)
-            res += self.countPali(s, i, i + 1)
+            res += self.countPali(s, i, i) # daab
+            res += self.countPali(s, i, i + 1) #daaab or dab
         return res
 
     def countPali(self, s, l, r):
         res = 0
         while l >= 0 and r < len(s) and s[l] == s[r]:
             res += 1
+            #keep expanding outwards , considering s[l] = s[r]
             l-= 1
             r += 1
         return res
