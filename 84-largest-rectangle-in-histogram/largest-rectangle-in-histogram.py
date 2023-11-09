32bcbeddf6d83440d25bfaedb7ccb55ec9d32db6
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-09 19:16:34 +0530

    Update largest-rectangle-in-histogram.py

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -9,9 +9,9 @@ class Solution:
             start = i
             while stack and stack[-1][1] > h:
                 index, height = stack.pop()
-                ## say in heights=[5,2]
-                ## at index 1,height 2 can extend even on left side to index 0,so we pop (0,5) and use 0 as index for height 2 in stack
-                maxArea = max(maxArea, height * (i - index)) #len*breadth
+                ## say in heights=[2,5]
+                ## at index 1,height 5 can extend even on left side to index 0,so we pop (0,2) and use 0 as index for height 5 in stack
+                maxArea = max(maxArea, height * (i - index)) #len*breadth ,where breadth is difference b/w 2 max heights' indices (current -past)
                 start = index                        #   /
             ## maintains monotonically increasing stack /
             stack.append ((start, h)) 
