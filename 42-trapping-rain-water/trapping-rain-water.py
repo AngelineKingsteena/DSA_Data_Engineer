00fdc424d54a4c60032abadaa30a51987e49bc10
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-09 18:51:51 +0530

    Update trapping-rain-water.py

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,5 +1,6 @@
 class Solution:
     ## video solution--https://www.youtube.com/watch?v=ZI2z5pq0TqA&ab_channel=NeetCode
+    ## idea :- min(left,right)- h[i]
     def trap(self, height: List[int]) -> int:
         if not height: return 0
         l, r = 0, len (height) - 1
@@ -8,8 +9,9 @@ class Solution:
         while l < r:
             if leftMax <= rightMax:
                 l += 1
+                # to avoid negative at index 0 in eg [0,1,0,2]
                 leftMax = max(leftMax, height[l])
-                res += leftMax - height [l]
+                res += leftMax - height [l] # at index 0 in eg [0,1,0,2], it'll be 1-1=0
             else:
                 r -= 1
                 rightMax = max(rightMax, height[r])
