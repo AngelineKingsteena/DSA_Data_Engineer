6e865139b38958c83e63e8429cc62682e26fa0ee
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-13 09:54:04 +0530

    Update merge-intervals.py

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -5,9 +5,9 @@ class Solution:
         intervals.sort(key = lambda i : i[0])
         output = [intervals [0]]
         for start, end in intervals[1:]:
-            lastEnd = output [-1][1]
-            if start <= lastEnd:
-                output [-1][1] = max(lastEnd, end)
+            latestEnd = output [-1][1]
+            if start <= latestEnd:
+                output [-1][1] = max(latestEnd, end)
             else:
                 output.append([start, end])
         return output
