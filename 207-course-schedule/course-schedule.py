79be459a1c48ede019927def03d889048f0ab269
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-15 18:16:01 +0530

    Update course-schedule.py

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,7 +2,10 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # https://www.youtube.com/watch?v=EgI5nU9etnU&ab_channel=NeetCode
         # map each course to prereq list
+        ## eg:-numCourses=2,prerequisites=[[1,0],[0,1]]
+        ## preMap = {0: [1], 1: [0]}
         preMap = { i: [] for i in range (numCourses) }
+        
         for crs, pre in prerequisites:
             preMap [crs].append (pre)
         
@@ -16,12 +19,18 @@ class Solution:
             
             visitSet.add(crs)
             for pre in preMap[crs]:
-                if not dfs(pre): return False
+                if not dfs(pre): 
+                    return False
+                else:
+                    continue
             visitSet.remove(crs)
             preMap[crs] = []
             return True
         for crs in range(numCourses) :
-            if not dfs(crs): return False
+            if not dfs(crs): 
+                return False
+            else:
+                continue
         return True
         # 1 -> 2
         # 3 -> 4
