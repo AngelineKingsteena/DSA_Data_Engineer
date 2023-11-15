fddf8095684631922cf5c39eeafdb4fa94497c31
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-15 10:44:12 +0530

    Update maximum-product-subarray.py

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -8,9 +8,13 @@ class Solution:
             res = max(res, curMax)
         return res
 
-# Subproblem for the DP here would be: What is the maximum and minimum product we can get for a contiguous sub-array starting from the 0th to the current element? Why do we need to maintain the minimum product while we are asked for a maximum? The fact is that elements in nums can be negative, so it possible that for some negative element the previous min possible product can turn the current product into a greater value.
+# Subproblem for the DP here would be: What is the maximum and minimum product we can get for a contiguous sub-array 
+# starting from the 0th to the current element? 
+#Why do we need to maintain the minimum product while we are asked for a maximum? 
+# The fact is that elements in nums can be negative, so it possible that for some negative element the previous min 
+# possible product can turn the current product into a greater value.
 # example:
-[-1,-2,-3]
+# [-1,-2,-3]
  ## in [-1,-2] min is -2,max 2
  ## in [-2,-3] min is -6,max 6
 # Time: O(n) - scan
