9b66b09c19b58e3bb56fcd6a18babb77b98a8791
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-16 17:07:40 +0530

    Update rotate-image.py

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -10,19 +10,22 @@ class Solution:
         # ↑    .  as u can see value in bottom left  will keep
         # d...←c  moving upward,hence matrix[bottom - i][l]
         l, r = 0, len(matrix) - 1
+        top, bot = l, r
         while l < r:
-            for i in range(r - l):
-                top, bottom = l,r
+            for i in range(r-l):
                 # save the topleft
-                topLeft = matrix[top][l + i]
+                topleft = matrix[top][l + i]
                 # move bottom left into top left
-                matrix[top][l + i] = matrix[bottom - i][l]
+                matrix[top][l + i] = matrix[bot - i][l]
                 # move botton right into bottom left
-                matrix[bottom - i][l] = matrix[bottom][r - i]
+                matrix[bot - i][l] = matrix[bot][r - i]
                 # move top right into botton right
-                matrix[bottom][r - i] = matrix[top + i][r]
+                matrix[bot][r - i] = matrix[top + i][r]
                 # move top left into top right
-                matrix[top + i][r] = topLeft
-            r-=1
-            l+=1
+                matrix[top + i][r] = topleft
+            r -= 1
+            l += 1
+            top += 1
+            bot -= 1
+
                 
