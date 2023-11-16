62238fd2fac558cdd5fd984ad1d905cbd262a39b
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-16 09:56:17 +0530

    Update coin-change.py

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -19,7 +19,7 @@ class Solution:
 # coins = [1,2,5], amount = 5
 
 # Initialize dp as [0, 6, 6, 6, 6, 6] (size= amount + 1).
-## each element in dp represents an no.of ways to make index amount 
+## each element in dp represents an no.of coins required to make index amount 
 # [N for re.0,N for rs.1,N for rs.2..etc]..where N=min coins requeuired...
 
 # For each coin denomination (1, 2, and 5):
