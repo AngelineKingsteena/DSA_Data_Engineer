eec2952e072b0cce2fdf4996d01199b3469acbd0
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-16 19:49:24 +0530

    Update merge-intervals.py

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -2,6 +2,7 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         # https://www.youtube.com/watch?v=44H3cEC2fFM&ab_channel=NeetCode
         # O(nlogn)
+        ## sort by start time
         intervals.sort(key = lambda i : i[0])
         output = [intervals [0]]
         for start, end in intervals[1:]:
