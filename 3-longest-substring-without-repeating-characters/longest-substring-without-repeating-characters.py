37ae8273c31c694936a46e6251376cf32f02578b
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-16 08:30:57 +0530

    Update longest-substring-without-repeating-characters.py

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,9 +5,11 @@ class Solution:
         l = 0
         res = 0
         for r in range(len (s)):
+            ## keep moving left pointer and removing prior occurances of said characters identified by left pointer
             while s[r] in charSet:
-                charSet.remove(s [l])
+                charSet.remove(s [l]) ## note s[l] and not s[r]-->s[l] points to prior occurances of said character
                 l += 1
+            ## post duplicate handling by left pointer,progress through with right pointer
             charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
