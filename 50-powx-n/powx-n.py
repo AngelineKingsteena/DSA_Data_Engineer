1c1168a8ac8ac0884d6994ecef4f5a3e9e476a59
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-16 19:04:05 +0530

    Update powx-n.py

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,6 +1,7 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         # https://www.youtube.com/watch?v=g9YQyYi4IQQ&ab_channel=NeetCode
+        ## idea : break the power down as multiples of 2
         def helper(x, n) :
             if x == 0: return 0
             if n == 0: return 1
