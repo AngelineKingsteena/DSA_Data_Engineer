9e34a6a50987c1c27b7d9d5d6bfbc6edefaa3703
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 08:54:05 +0530

    Update balanced-binary-tree.py

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,6 +7,8 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         ## video solution :-https://www.youtube.com/watch?v=QfJsau0ItOY&ab_channel=NeetCode
+        ## A balanced binary tree or height-balanced binary tree is such a tree whose left and right subtree's height differ by not
+        ## more than 1, which means the height difference could be -1, 0, and 1. A balanced binary tree is also known as a height-balanced tree.
         def dfs(root):
             if not root:
                 ### return [balanced? bool,height] 
