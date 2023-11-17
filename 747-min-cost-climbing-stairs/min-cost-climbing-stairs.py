234fee34169d796c78f6734e17319ceadfad5930
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 11:34:15 +0530

    Update min-cost-climbing-stairs.py

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -7,6 +7,7 @@ class Solution:
         cost=c.copy()
         cost.append(0)
         for i in range(len (cost) - 3, -1, -1):
+            #### min(15+20,15+0)
             cost[i] += min (cost[i + 1], cost[i + 2])
         return min(cost [0], cost [1]) #because question says:-You can either start from
         #the step with index 0, or the step with index 1.
