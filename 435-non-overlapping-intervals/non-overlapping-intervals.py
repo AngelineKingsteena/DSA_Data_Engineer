fb49e15c13cc43390c2745e1b6607c503a1d64cb
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 10:41:55 +0530

    Update non-overlapping-intervals.py

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -5,9 +5,14 @@ class Solution:
         prevend=intervals[0][1]
         count=0
         for start,end in intervals[1:]:
+             ## if next interval starts before with prev interval's end
+            ##  prev  --------        
+            ##  curr      ----------   
             if start<prevend:
                 count+=1
                 prevend=min(prevend,end)
             else:
+            ##  prev  --------        or           ------
+            ##  curr  --------                --------
                 prevend=end
         return count
