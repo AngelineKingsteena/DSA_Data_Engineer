2e575841f9f2b69541738ebc15ccd48940780ae0
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 10:31:45 +0530

    Update best-time-to-buy-and-sell-stock-with-cooldown.py

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,8 +2,6 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 # https://www.youtube.com/watch?v=I7j0F7AHpb8&ab_channel=NeetCode
         # State: Buying or Selling?
-        # If Buy -> i + 1
-        # If Sell -› i + 2 coz after selling there's a compulsory cooldown
         cache = {} # key=(i, buying) val=max_profit
         def dp(i, buying) :
             if i >= len (prices):
@@ -12,9 +10,11 @@ class Solution:
                 return cache[(i, buying)]#buying is only available 
             cooldown = dp(i + 1, buying)
             if buying:
+                # If Buy -> i + 1
                 buy = dp(i + 1, not buying) - prices[i]#not buying,coz you can only either sell or cool down
                 cache[ (i, buying)] = max (buy, cooldown)
             else:
+                # If Sell -› i + 2 coz after selling there's a compulsory cooldown
                 sell = dp(i + 2, not buying) + prices [i]#not buying,coz you can cool down
                 cache[(i, buying)] = max (sell, cooldown)
             return cache[(i, buying)]
