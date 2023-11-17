cdc84e145f752b118a200babae0e200935ec96fa
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 11:14:41 +0530

    Update edit-distance.py

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,6 +1,14 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
 # https://www.youtube.com/watch?v=XYi2-LPrwm4&ab_channel=NeetCode
+        ## no.rows=len(word1) + 1
+        ## no.columns=len(word2) + 1
+        #          w2
+        #       a c d ""
+        # w1  a       3
+        #     b       2
+        #     d   1 0 1
+        #     " 3 2 1 0
         cache = [[float("inf")] * (len (word2) + 1) for i in range(len(word1) + 1)]
         for j in range(len (word2) + 1):
             cache[len (word1)][j] = len(word2) -j
