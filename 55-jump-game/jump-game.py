dc4221eaf4e1e8e40f382c776f9fe9d40438aae1
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 10:47:13 +0530

    Update jump-game.py

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -4,6 +4,7 @@ class Solution:
         goal = len(nums) - 1
         for i in range(len (nums) - 1, -1, -1):
             if i + nums[i] >= goal:
+                ## move goal inwards(towards left) to the index which helped reach prior(farther right) goal
                 goal = i
         return goal == 0
         
