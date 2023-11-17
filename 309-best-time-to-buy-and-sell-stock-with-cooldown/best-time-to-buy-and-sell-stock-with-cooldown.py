7d648168ad90f8370892d2c25823b6e5aeb2f28d
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 10:30:57 +0530

    Update best-time-to-buy-and-sell-stock-with-cooldown.py

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,19 +4,19 @@ class Solution:
         # State: Buying or Selling?
         # If Buy -> i + 1
         # If Sell -› i + 2 coz after selling there's a compulsory cooldown
-        dp = {} # key=(i, buying) val=max_profit
-        def dfs(i, buying) :
+        cache = {} # key=(i, buying) val=max_profit
+        def dp(i, buying) :
             if i >= len (prices):
                 return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]#buying is only available 
-            cooldown = dfs(i + 1, buying)
+            if (i, buying) in cache:
+                return cache[(i, buying)]#buying is only available 
+            cooldown = dp(i + 1, buying)
             if buying:
-                buy = dfs(i + 1, not buying) - prices[i]#not buying,coz you can only either sell or cool down
-                dp[ (i, buying)] = max (buy, cooldown)
+                buy = dp(i + 1, not buying) - prices[i]#not buying,coz you can only either sell or cool down
+                cache[ (i, buying)] = max (buy, cooldown)
             else:
-                sell = dfs(i + 2, not buying) + prices [i]#not buying,coz you can cool down
-                dp[(i, buying)] = max (sell, cooldown)
-            return dp[(i, buying)]
-        return dfs(0, True)
+                sell = dp(i + 2, not buying) + prices [i]#not buying,coz you can cool down
+                cache[(i, buying)] = max (sell, cooldown)
+            return cache[(i, buying)]
+        return dp(0, True)
                 
