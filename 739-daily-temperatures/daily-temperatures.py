bb80ad4c76b18faf5759b76ae401919d533f71e9
Angeline Kingsteena <angelinekingsteena@gmail.com>
2023-11-17 15:45:38 +0530

    Update daily-temperatures.py

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -4,9 +4,9 @@ class Solution:
         res = [0] * len(temperatures)
         stack = [] # pair: [temp, index]
         for i, t in enumerate (temperatures):
-            while stack and t > stack[-1][0]:
-                stackT, stackInd = stack. pop()
+            while stack and t > stack[-1][0]:#once you meet next highest
+                stackT, stackInd = stack. pop() ## keep popping until u get to end of stack/in res[] index of previous highest
                 res [stackInd] = (i - stackInd)
-            stack.append([t, i])
+            stack.append([t, i]) ## keep adding without popping until u get next highest
         return res
         
