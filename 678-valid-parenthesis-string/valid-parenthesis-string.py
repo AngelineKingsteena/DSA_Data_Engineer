c5adf0765ba3d547245a03d9d89e28e931fce9d0
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-07 03:34:33 +0530

    Update valid-parenthesis-string.py

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,7 +1,15 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         # https://www.youtube.com/watch?v=QhPdNS143Qg&ab_channel=NeetCode
-        # quick understable in java:- https://youtu.be/vMbNfwfPecM?feature=shared
+        # quick understable in java from 7:00:- https://youtu.be/vMbNfwfPecM?feature=shared
+        """ leftMin: Represents the minimum count of open parentheses 
+        that still need to be closed properly to maintain the validity 
+        of the string. It is used to ensure that all closing parentheses
+        have a corresponding open parenthesis to match.
+        leftMax: Represents the maximum count of open parentheses
+        that could potentially be closed. It accounts for the
+        flexibility introduced by '*' characters, which can act 
+        as either open or close parentheses. """
         leftMin, leftMax = 0,0
         for c in s:
             if c =="(" :
