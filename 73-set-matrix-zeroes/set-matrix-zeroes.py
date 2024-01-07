db3a6368acaaf6a4b879fe9aaf8a8dd157225f09
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-07 07:49:24 +0530

    Update set-matrix-zeroes.py

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,11 +1,19 @@
 class Solution:
     """
-1)FLAG EXISTENCE OF ZERO IN OUTER PERIPHERAL:iterate over rows,see if any element in 0,column is 0,update,firstcol=True
-2)FLAG EXISTENCE OF ZERO IN OUTER PERIPHERAL:iterate over columns,see if any elemnt in 0,row is 0,update firstrow=True
-3)FLAG EXISTENCE OF ZERO IN INNER PERIPHERAL:iterate through rows and columns from 0 index,if any 1 elemnt[i][j] is 0,set entire row [i][0] and entire column[j][0]=0
-4)BASED ON FLAGS CHANGE VALUES(FROM ALL rows and columns EXCEPT FIRST ROW AND FIRST COLUMN) INTO 0:-iterate through ALL rows and columns EXCEPT FIRST ROW AND FIRST COLUMN I.E from index1,if [i][0] or [0][j]==0,set [i][j]=0
-5)BASED ON FLAGS CHANGE VALUES FROM FIRST COLUMNS INTO 0:-if firstcol=True,iterate through rows and set entire col=0,i.e,[i][0]=0
-6)BASED ON FLAGS CHANGE VALUES FROM FIRST ROW INTO 0:-if firstrow=True,iterate through cols and set entire row=0,i.e,[0][j]=0
+1)FLAG EXISTENCE OF ZERO IN OUTER PERIPHERAL:iterate over rows,see if any
+element in 0,column is 0,update,firstcol=True
+2)FLAG EXISTENCE OF ZERO IN OUTER PERIPHERAL:iterate over columns,see
+if any elemnt in 0,row is 0,update firstrow=True
+3)FLAG EXISTENCE OF ZERO IN INNER PERIPHERAL:iterate through rows and 
+columns from 0 index,if any 1 elemnt[i][j] is 0,set entire row [i][0]
+and entire column[j][0]=0
+4)BASED ON FLAGS CHANGE VALUES(FROM ALL rows and columns EXCEPT FIRST
+ROW AND FIRST COLUMN) INTO 0:-iterate through ALL rows and columns EXCEPT 
+FIRST ROW AND FIRST COLUMN I.E from index1,if [i][0] or [0][j]==0,set [i][j]=0
+5)BASED ON FLAGS CHANGE VALUES FROM FIRST COLUMNS INTO 0:-if firstcol=True
+,iterate through rows and set entire col=0,i.e,[i][0]=0
+6)BASED ON FLAGS CHANGE VALUES FROM FIRST ROW INTO 0:-if firstrow=True,
+iterate through cols and set entire row=0,i.e,[0][j]=0
 
 """
     def setZeroes(self,A):
