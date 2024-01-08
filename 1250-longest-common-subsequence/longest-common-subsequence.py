f78c9c872d8aeb38cd48580ab5c4821b5589c0af
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-08 16:46:48 +0530

    Time: 1503 ms (10.79%) | Memory: 309.7 MB (13.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    # def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         # https://www.youtube.com/watch?v=Ua0GhsJSlWM&ab_channel=NeetCode
         # T.C & S.C : O (len(str1)*len(str2))
         ##idea:-
@@ -22,13 +22,31 @@ class Solution:
 #    " "|0 | 0| 0
 
 # range(len(text2) + 1) becoz that extra 0
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len (text2) - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    ## diagonal--since this pair is matching,we can move diagonal to next pair of letters
-                    dp[i][j] = 1 + dp[i + 1][j + 1] #(1+dia,coz first dia is always 0)
-                else:
-                    ## max(right,bottom)
-                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
-        return dp[0][0]
+        # dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        # for i in range(len(text1) - 1, -1, -1):
+        #     for j in range(len (text2) - 1, -1, -1):
+        #         if text1[i] == text2[j]:
+        #             ## diagonal--since this pair is matching,we can move diagonal to next pair of letters
+        #             dp[i][j] = 1 + dp[i + 1][j + 1] #(1+dia,coz first dia is always 0)
+        #         else:
+        #             ## max(right,bottom)
+        #             dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+        # return dp[0][0]
+
+    def longestCommonSubsequence(self,text1: str, text2: str) -> int:
+        cache = {}
+
+        def dp(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if (i, j) in cache:
+                return cache[(i, j)]
+
+            if text1[i] == text2[j]:
+                cache[(i, j)] = 1 + dp(i + 1, j + 1)
+            else:
+                cache[(i, j)] = max(dp(i, j + 1), dp(i + 1, j))
+
+            return cache[(i, j)]
+
+        return dp(0, 0)
