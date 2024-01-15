9b3a46f8d2ff8542c25db9fc83a445c3bfdfe755
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-15 06:23:31 +0530

    Update longest-increasing-subsequence.py

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -18,10 +18,13 @@ class Solution:
         # Check if it is in increasing order if it is then change the value of dp[i] to max between dp[i] or 1+dp[j]. After that return max of dp.
 
 # lets take an example of nums = [1,2,4,3]
-# dp[3] would be 1 because the longest sub starting from the end of the array would be 1. dp[2] would be 1 if nums[2]>nums[3] and because then it would not be
-# increasing and dp[2] would be 1+dp[3] because then we have to include the last element too. After that dp[1] would be 1 if nums[1]>nums[2] because then again
-# the sub would not be increasing and dp[1] would 1+dp[2] if we include it. After that in the case of dp[0] we will do that thing again and then our final
-        #dp array would be [3,2,1,1] ([1+dp[1],1+dp[2],dp[2],dp[3]]) and our answer would be max of dp i.e 3.
+# dp[3] would be 1 because the longest sub starting from the end of the array would be 1. 
+#dp[2] would be 1 if nums[2]>nums[3] and because then it would not be
+# increasing and dp[2] would be 1+dp[3] because then we have to include the last element too.
+#After that dp[1] would be 1 if nums[1]>nums[2] because then again
+# the sub would not be increasing and dp[1] would 1+dp[2] if we include it. 
+#After that in the case of dp[0] we will do that thing again and then our final
+#dp array would be [3,2,1,1] ([1+dp[1],1+dp[2],dp[2],dp[3]]) and our answer would be max of dp i.e 3.
 
 # Complexity
 # Time complexity:O(n^2)
