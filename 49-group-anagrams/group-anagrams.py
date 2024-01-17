61ac41eccf15824eaae2aed6fadc31d42ef81d50
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-17 16:44:05 +0530

    Time: 83 ms (92.83%) | Memory: 20.8 MB (49.00%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
       # Hash Map
-        hash_map = {}
+        hash_map = defaultdict(list)
         # Iterate over each string in given list
         for s in strs:
             # Hash function to get key
             key = ''.join(sorted(s))
             # CASE: If key is in hash map
-            if key in hash_map:
-                hash_map[key].append(s)
-            # CASE: If key is not in hash map
-            else:
-                hash_map[key] = [s]
+            hash_map[key].append(s)
         return hash_map.values()
 
 # OR SHORTENED VERSION USING DEFAULTDICT
