6389c3e984b49d64d299d54ac968da66fe46c546
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-17 06:52:57 +0530

    Update merge-triplets-to-form-target-triplet.py

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -7,6 +7,8 @@ class Solution:
         for t in triplets:
            ## ignore the triplet which has ith element greater than ith element
            ## in target,even if one index value is greater,completely ignore full triplet
+            ## example expected=False for eg: triplets = [[3,5,1],[10,5,7]],target =[3,5,7]
+            ## in eg target can't be formed coz 2nd triplet is completely ignore coz 10 > target's 3 at 0th index
             if t[0] > target[0] or t[1] > target[1] or t[2] > target [2]:
                 continue
             for i, v in enumerate(t):
