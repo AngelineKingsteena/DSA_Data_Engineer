29c74c17c0dccbba431b8143a147ee94ac1ee40e
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-17 16:17:00 +0530

    Update jump-game-ii.py

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -3,9 +3,9 @@ class Solution:
         # https://www.youtube.com/watch?v=dJ7sWiOoK7g&ab_channel=NeetCode
         res = 0
         l=r=0
-        while r < len(nums) - 1:
+        while r < len(nums) - 1: ## limit is len(nums)-1 to avoid out of bounds in later l=r+1
             farthest = 0
-            for i in range(l, r + 1):
+            for i in range(l, r + 1): # here r+1,to include r also in farthest calculation according to logic
                 farthest = max(farthest, i + nums[i])
             l = r + 1
             r = farthest
