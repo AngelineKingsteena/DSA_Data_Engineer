a5a1f69b54bbb6eb5ca6cedf99c1339f23a5b4ae
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-18 12:58:43 +0530

    Update car-fleet.py

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -4,7 +4,8 @@ class Solution:
         pair = [[p, s] for p, s in zip(position, speed)]
         stack = []
         # sorted and reversed coz, the car closeest to destination will anyway block the rest of the cars behind it to maybe form a fleet,if they catch up
-        for p, s in sorted(pair)[::-1]: # Reverse Sorted Order
+        for p, s in sorted(pair)[::-1]: # Reverse Sorted Order,BCOZ OF WHICH FIRST ELEMENT(TIME) ADDED IN STACK IS ALREADY LESSER
+            ## SO IF EVEN LESSER VALUE [-1] COMES POP IT OUT
             stack.append((target - p) / s) # time taken
             # the car which is coming behind (stack[-2])  takes more time (indirectly is slower )
             ## than closest car,so will become a fleet with recent closest (stack[-1])
