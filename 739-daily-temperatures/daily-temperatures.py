114c72d3a0d7b8babf4962f1559b3f6ce82e37fa
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-18 09:21:16 +0530

    Update daily-temperatures.py

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -6,7 +6,9 @@ class Solution:
         for i, t in enumerate (temperatures):
             while stack and t > stack[-1][0]:#once you meet next highest
                 stackT, stackInd = stack. pop() ## keep popping until u get to end of stack/in res[] index of previous highest
+                # update res at popped index with value of diff bet. current max temp's index and popped index
                 res [stackInd] = (i - stackInd)
+            ## after above cal,then add cuurent max tmp & index
             stack.append([t, i]) ## keep adding without popping until u get next highest
         return res
         
