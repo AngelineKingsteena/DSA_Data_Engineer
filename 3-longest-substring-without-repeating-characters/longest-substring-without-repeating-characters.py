467c8b072ff6ec0778e5757f8fb19dfc04d2916b
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-19 06:28:39 +0530

    Update longest-substring-without-repeating-characters.py

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,11 +5,15 @@ class Solution:
         l = 0
         res = 0
         for r in range(len (s)):
-            ## keep moving left pointer and removing prior occurances of said characters identified by left pointer
+            ## we're not looking for subset,rather substring,so "baabc",at iteration r=2,l will remove 0th b & 1th a,
+            ## so charset will become empty in while loop
+            ## outside while loop charset would get s[2]=a
             while s[r] in charSet:
                 charSet.remove(s [l]) ## note s[l] and not s[r]-->s[l] points to prior occurances of said character
                 l += 1
-            ## post duplicate handling by left pointer,progress through with right pointer
+            ## post  handling substrings which had duplicate element 
+            ## (eg: in "'acd'd",acd will be removed by while coz last d is a duplicate occurence ) 
+            ## by left pointer,progress through with right pointer
             charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
