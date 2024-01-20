75726da9b66fa7ccdd881443324148ba9b963dd1
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-20 17:18:31 +0530

    Time: 40 ms (34.45%) | Memory: 16.5 MB (65.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,13 +5,24 @@ class Solution:
         ## t.c :-n
         #[newOne,newTwo/oldOne,oldTwo]
         #[1+1,1,1]
-        one, two = 1, 1 #two is no. of ways to reach top from top
-         #one is no. of ways to reach top from top before step
-        ## skip last two
-        for i in range(n - 1):
-            # temp = one
-            # one = one + two
-            # two = temp
-            one,two=one + two,one
-        return one
+        # one, two = 1, 1 #two is no. of ways to reach top from top
+        #  #one is no. of ways to reach top from top before step
+        # ## skip last two
+        # for i in range(n - 1):
+        #     # temp = one
+        #     # one = one + two
+        #     # two = temp
+        #     one,two=one + two,one
+        # return one
+        # Edge cases
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        dp=[0]*(n-2) +[2,1]
+        print(dp)
+
+        for i in range(n-3,-1,-1):
+            dp[i]=dp[i+1]+dp[i+2]
+        return dp[0]
         
