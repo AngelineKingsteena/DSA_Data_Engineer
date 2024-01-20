2a9e757a76fa0e6c7662081152d5bf6aee3aad6b
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-20 18:57:44 +0530

    Update house-robber.py

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -10,4 +10,13 @@ class Solution:
             ## its a + nums[i] and not b + nums[i],cause alternative indices,eg:-a is at 0th index,i is at 2nd index
             a, b = b, max(a + nums[i], b)
         return b
+        # # Initialize a DP array to store the maximum sum at each house
+        # dp = [0] * len(nums)
+        # dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
+        # # Fill in the DP array starting from the third house
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        # return dp[-1]
         
