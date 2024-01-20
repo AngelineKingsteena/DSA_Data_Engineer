34f4143aa1309859a7529e74a8aa8c4937afc185
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-20 15:36:53 +0530

    Update subarray-sum-equals-k.py

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -9,6 +9,8 @@ class Solution:
         for n in nums:
             curSum += n
             diff = curSum - k
+            ## coz indirectly it implies, if loose these many counts of diff(prefixSums.get(diff, 0)),
+            ## I'd get target sum,meaning,these counts of target sum is possible
             res += prefixSums.get(diff, 0)
             prefixSums [curSum] = 1 + prefixSums.get (curSum, 0)
         return res
