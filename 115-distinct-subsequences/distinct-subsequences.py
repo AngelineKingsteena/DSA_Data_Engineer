d3e3df45237751b363ea382634342e10bd7f0af6
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-21 18:55:34 +0530

    Update distinct-subsequences.py

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -7,7 +7,7 @@ class Solution:
         # Initialize the matrix with base cases
         ## places where t is empty
         for i in range(m + 1):
-            ## all rows of column n
+            ## all rows of column n--> t is over and now nth column is "" char
             dp[i][n] = 1
 
         # Iterate through the matrix to fill in the values
