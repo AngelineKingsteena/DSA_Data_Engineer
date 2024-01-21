f7da77346375fce7e5ace837d7e122f5e073278a
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-21 16:59:11 +0530

    Update daily-temperatures.py

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,6 +1,7 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         ## video solution :-https://www.youtube.com/watch?v=cTBiBSnjO3c&ab_channel=NeetCode
+        ## only store next heighest temp ...implicitly means to pop lesser temp
         res = [0] * len(temperatures)
         stack = [] # pair: [temp, index]
         for i, t in enumerate (temperatures):
