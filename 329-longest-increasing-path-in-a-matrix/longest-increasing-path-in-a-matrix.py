7f67b348ddf9cb5f87cc3d4f76641cb304a09cf4
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-21 19:20:12 +0530

    Time: 387 ms (43.12%) | Memory: 20.8 MB (45.40%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,37 +1,37 @@
+from typing import List
+
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        # https://www.youtube.com/watch?v=wCc_nd-GiEc&ab_channel=NeetCode
-        ROWS, COLS = len (matrix), len(matrix[0])
-        dp = {} # (r, c)-->LIP
+        ROWS, COLS = len(matrix), len(matrix[0])
+        visited = set()  # (r, c) tuples to keep track of visited cells
+        memo = {}  # Memoization dictionary
+
         def dfs(r, c, prevVal):
             if (r < 0 or r == ROWS or
-            c < 0 or c == COLS or
-            matrix[r][c] <= prevVal):
+                    c < 0 or c == COLS or
+                    matrix[r][c] <= prevVal or
+                    (r, c) in visited):
                 return 0
-            if (r, c) in dp:
-                return dp[(r, c)]
-            """
-            max becoz in input
-            9,9,4
-            6,6,8
-            2,1,1
-            the dfs lip matrix looks like below, wherein each element 
-            indicates lip from itself that's 1+x
-            1,1,2
-            2,2,1
-            3,4,2
-            the last middle one in i/p can go up or right
-            max(up (1+2), right(1+3))
-            """
+
+            if (r, c) in memo:
+                return memo[(r, c)]
+
+            visited.add((r, c))
+
             res = 1
-            res = max(res, 1 + dfs(r+1,c,matrix[r][c]))
-            res = max(res, 1 + dfs(r-1,c,matrix[r][c]))
-            res = max(res, 1 + dfs(r,c+1,matrix[r][c]))
-            res = max(res, 1 + dfs(r,c-1,matrix[r][c]))
-            dp[(r, c)] = res
+            res = max(res, 1 + dfs(r + 1, c, matrix[r][c]))
+            res = max(res, 1 + dfs(r - 1, c, matrix[r][c]))
+            res = max(res, 1 + dfs(r, c + 1, matrix[r][c]))
+            res = max(res, 1 + dfs(r, c - 1, matrix[r][c]))
+
+            visited.remove((r, c))
+            memo[(r, c)] = res
+
             return res
-        for r in range(ROWS) :
+
+        max_lip = 0
+        for r in range(ROWS):
             for c in range(COLS):
-                dfs(r, c, -1)
-        return max(dp. values ())
-                
+                max_lip = max(max_lip, dfs(r, c, -1))
+
+        return max_lip
