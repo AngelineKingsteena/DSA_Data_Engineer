9bf7d9dd904e3d6d6f689dcee4df423a5b0374b6
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-01-21 17:27:38 +0530

    Update car-fleet.py

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,8 +1,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         ## video solution :-https://www.youtube.com/watch?v=Pr6T-3yB9RM&ab_channel=NeetCode
-        ### keep only higher spped cars,by popping lower speed one,
-        ## if final result stack is [2,9,19]--- it means 3 fleets wherein [x cars had speed<=2,y cars had speed<=9,z cars had speed<=19]
+        ### keep only higher time cars,by popping lower time one,
+        ## if final result stack is [2,9,19]--- it means 3 fleets wherein [x cars had time<=2,y cars had time<=9,z cars had time<=19]
         ## where x,y,z can be 1 or more
         pair = [[p, s] for p, s in zip(position, speed)]
         stack = []
@@ -18,6 +18,7 @@ class Solution:
             ## basically keep storing cars in ascending order of time taken in stack by popping the least time taking
         return len(stack)
         
-### regardless of faster/slower,the speed of car closest to target is first added into stack,
-        # so any car slower than this speed will end up creating a seperate fleet,
-        # else if theyre faster,they'll merge with the car in front of it,so pop out speed of front car
+### regardless of faster/slower,the time of car closest to target is first added into stack,
+# so any car taking more time than this speed will end up creating a seperate fleet,
+# else if theyre taking lesser time,they'll merge with the car in front of it,so pop out time of front car
+# pop here means remove prior blocking,new car blockage has arrived.
