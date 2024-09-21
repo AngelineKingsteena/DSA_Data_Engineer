b4cf0f5a544c6a6d310e3b5d5e993a5336cb69fb
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-09-21 14:34:57 +0530

    Time: 54 ms (64.70%) | Memory: 16.7 MB (8.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,20 +1,30 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ## video solution :-https://www.youtube.com/watch?v=wiGpQwVHdE0&ab_channel=NeetCode
-        charSet = set()
-        l = 0
-        res = 0
-        for r in range(len (s)):
-            ## we're not looking for subset,rather substring,so "baabc",at iteration r=2,l will remove 0th b & 1th a,
-            ## so charset will become empty in while loop
-            ## outside while loop charset would get s[2]=a
-            while s[r] in charSet:
-                charSet.remove(s [l]) ## note s[l] and not s[r]-->s[l] points to prior occurances of said character
-                l += 1
-            ## post  handling substrings which had duplicate element 
-            ## (eg: in "'acd'd",acd will be removed by while coz last d is a duplicate occurence ) 
-            ## by left pointer,progress through with right pointer
-            charSet.add(s[r])
-            res = max(res, r - l + 1)
-        return res
+        # def lengthOfLongestSubstring(s: str) -> int:
+        i, j = 0, 0  # Two pointers for the sliding window
+        N = len(s)  # Length of the string
+        cnt = {}  # Dictionary to count occurrences of characters
+        dup = 0  # To track the number of duplicate characters
+
+        while j < N:
+            # Increment the count of the current character
+            cnt[s[j]] = cnt.get(s[j], 0) + 1
+            # Increment duplicates if this character count becomes 2
+            if cnt[s[j]] == 2:
+                dup += 1
+            
+            # If there are duplicates, increment i to shrink the window
+            if dup > 0:
+                if cnt[s[i]] == 2:
+                    dup -= 1
+                cnt[s[i]] -= 1
+                i += 1
+            
+            j += 1  # Move the right pointer to expand the window
+
+        return j - i  # Return the length of the longest substring
+
+# s = "abcabcbb"
+# print(lengthOfLongestSubstring(s))  # Output: 3
+
         
