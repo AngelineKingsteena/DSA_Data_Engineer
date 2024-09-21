534f923c93325dfb8ff9aedde726fb0b33f09153
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-09-21 15:52:32 +0530

    Time: 148 ms (44.52%) | Memory: 17.2 MB (77.41%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,48 +1,47 @@
+from collections import defaultdict
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        ## video solution :-https://www.youtube.com/watch?v=CX6_L9GLldU&ab_channel=TimothyHChang
+       
+        table = defaultdict(int)
 
-        """
-        Intuition:
+        # Initialize frequency table for t
+        for c in t:
+            table[c] += 1
 
-We maintain two pointers, start and end, to represent a window in the string s.
-Initially, we move the end pointer to the right, expanding the window, until we include all the characters from string t in the window.
-Then, we move the start pointer to the right, shrinking the window while still maintaining the requirement of having all characters from t. This helps us find the minimum window size that satisfies the condition.
-We keep track of the minimum window size and its corresponding substring as we iterate through the entire string s.
-        """
-        lookup = Counter(t)
-        miN = float("inf")
-        output = ""
-        S=len(s)
-        start,end = 0,0
-        count = len(lookup)
-        #ADOBECODEBANC
-        
-        while end < S:
-            #end pointer
-            while end < S and count!=0:
-                if s[end] in lookup:
-                    lookup[s[end]] -= 1
-                    if lookup[s[end]] == 0:
-                        count -= 1
-                end+=1
-            #start pointer
-            while start<end and count == 0:
-                if end-start < miN:
-                    miN = end-start
-                    output = s[start:end]
-                if s[start] in lookup:
-                    lookup[s[start]] += 1
-                    ## in "ADOBECODEBANC" if we need 1 count 
-                    ## of b and have 2 b's ,then
-                    ## lookup[s[start]]+=1 would have made -1 as 0, so as long 
-                    ## as we have enough just enough b's in o/p
-                    ## string (i.e lookup[s[start]]>0) then do
-                    ## count += 1,to move "end" to search in closer 
-                    ## vicinity of this extra b
-                    if lookup[s[start]]>0:
-                        count += 1
-                start+=1
-            
-        return output
-        
+        # Initialize sliding window
+        begin, end = 0, 0
+        counter = len(table)
+        len_min = float('inf')
+        ans = ""
+
+        # Start sliding window
+        while end < len(s):
+            endchar = s[end]
+
+            # If current char found in table, decrement count
+            if endchar in table:
+                table[endchar] -= 1
+                if table[endchar] == 0:
+                    counter -= 1
+
+            end += 1
+
+            # If counter == 0, means we found an answer, now try to trim that window
+            while counter == 0:
+                # Store new answer if smaller than previously best
+                if end - begin < len_min:
+                    len_min = end - begin
+                    ans = s[begin:end]
+
+                # Try to shrink the window from the left
+                startchar = s[begin]
+
+                if startchar in table:
+                    table[startchar] += 1
+                    if table[startchar] > 0:
+                        counter += 1
+
+                begin += 1
+
+        return ans
