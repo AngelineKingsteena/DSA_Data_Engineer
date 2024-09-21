23819b174d62ee31a958aa74a7cd05d576691086
Angeline Kingsteena <angelinekingsteena@gmail.com>
2024-09-21 16:04:04 +0530

    Time: 65 ms (53.05%) | Memory: 16.6 MB (87.05%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,25 +1,34 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        ## video solution :-https://www.youtube.com/watch?v=VIR699JK4Yg&ab_channel=PersistentProgrammer
-        lookup1 = [ord(i) - ord('a') for i in s1]
-        lookup2 = [ord(i) - ord('a') for i in s2]
-        # lookup1 = [0, 1] ab
-        # lookup2 = [4, 8, 3, 1, 0, 14, 14, 14] eidbaooo
-        ## target and window will use values of lookup1 &2 as indices,
-        ## and their values will be count of char
-        target = [0] * 26
-        window = [0] * 26
-        for freq in lookup1:
-            target[freq]+=1
-        # target = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        for i, freq in enumerate(lookup2):#[4, 8, 3, 1, 0, 14, 14, 14]
-            window[freq] += 1
-            ## to indicate already looked at [4,8] time to look at [8,3]
-            ## in [4, 8, 3, 1, 0, 14, 14, 14] eidbaooo ,for s1="ab"
-            if i >= len(s1): #window size exceeded
-                rem = i - len(s1) # at index 2,index 0 is out of window
-                window[lookup2[rem]] -= 1 
-            if window == target:
-                return True
+        table = defaultdict(int)
+
+        for c in s1:
+            table[c] += 1
+
+        begin, end = 0, 0
+        counter = len(table)
+
+        while end < len(s2):
+            endchar = s2[end]
+
+            if endchar in table:
+                table[endchar] -= 1
+                if table[endchar] == 0:
+                    counter -= 1
+
+            end += 1
+
+            while counter == 0:
+                if end - begin == len(s1):
+                    return True
+
+                startchar = s2[begin]
+
+                if startchar in table:
+                    table[startchar] += 1
+                    if table[startchar] > 0:
+                        counter += 1
+
+                begin += 1
+
         return False
-                
